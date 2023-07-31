8534333aa0798bd683e3d3060ea7ea9fbba0f962
Ayodele <ayodelearogundade12@gmail.com>
2023-07-31 19:24:29 +0100

    create positive or negative

---
+++ python-hello_world/2-positive_or_negative.py
@@ -0,0 +1,12 @@
+
+import random
+number = random.randint(-10, 10)
+def signChecker(number):
+    if(number > 0):
+        print(number,"is positive")
+    elif(number == 0):
+        print(number,"is zero")
+    else:
+        print(number,"is negative")
+
+signChecker(number)
