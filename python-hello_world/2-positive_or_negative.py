f98f5a0a882f4a2e04c57ce91800ee8d41e14793
Ayodele <ayodelearogundade12@gmail.com>
2023-08-02 18:30:37 +0100

    create last digit printer

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,12 +1,14 @@
-
 import random
 number = random.randint(-10, 10)
+
+
 def signChecker(number):
-    if(number > 0):
-        print(number,"is positive")
-    elif(number == 0):
-        print(number,"is zero")
+    if (number > 0):
+        print(number, "is positive")
+    elif (number == 0):
+        print(number, "is zero")
     else:
-        print(number,"is negative")
+        print(number, "is negative")
+
 
 signChecker(number)
---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,14 @@
+import random
+number = random.randint(-10000, 10000)
+numList = list(map(int, str(number)))
+def lastDigitPrinter(number):
+    firstWords = "Last digit of"
+    lastDigit = numList[-1]
+    if (lastDigit > 5):
+        print(firstWords, number, "is", lastDigit, "and is greater than 5")
+    elif (lastDigit == 0):
+        print(firstWords, number, "is", lastDigit, "and is 0")
+    elif (lastDigit < 6) and (lastDigit != 0):
+        print(firstWords, number, "is", lastDigit, "and is less than 6 and not 0")
+
+lastDigitPrinter(number)
